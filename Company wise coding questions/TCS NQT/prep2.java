24166543dee3fb4c28c393d07038c4aa4cc9a7fd
ImSayak <maitysayak07@gmail.com>
2024-02-02 21:59:10 +0530

    prep3 added in tcs nqt

---
+++ Company wise coding questions/TCS NQT/prep2.java
@@ -0,0 +1,31 @@
+/*
+Problem Statement – Given a string S(input consisting) of ‘*’ and ‘#’. The length of the string is variable. The task is to find the minimum number of ‘*’ or ‘#’ to make it a valid string. The string is considered valid if the number of ‘*’ and ‘#’ are equal. The ‘*’ and ‘#’ can be at any position in the string.
+Note : The output will be a positive or negative integer based on number of ‘*’ and ‘#’ in the input string.
+
+(*>#): positive integer
+(#>*): negative integer
+(#=*): 0
+Example 1:
+Input 1:
+
+###***   -> Value of S
+Output :
+
+0   → number of * and # are equal
+ */
+
+import java.util.*;
+public class prep2 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+        int res =0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='*')  res++;
+            else res--;
+        }
+        System.out.println(res);
+    }
+}
